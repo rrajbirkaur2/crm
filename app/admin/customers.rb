585e2b99e04b0bde1e245cff5a09685b6bdb3cd5
rrajbirkaur2 <rrajbirkaur2@academic.rrc.ca>
2026-03-09 12:07:37 -0500

    updated

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,14 +1,21 @@
 ActiveAdmin.register Customer do
- permit_params :full_name, :phone_number, :email, :notes, :image
+  permit_params :full_name, :phone_number, :email, :notes, :image
 
- form do |f|
-   f.inputs do
-     f.input :full_name
-     f.input :phone_number
-     f.input :email
-     f.input :notes
-     f.input :image, as: :file
-   end
-   f.actions
- end
+  # Only add filters you actually want
+  filter :full_name
+  filter :phone_number
+  filter :email
+  filter :notes
+  # DO NOT add filter :image
+
+  form do |f|
+    f.inputs "Customer Details" do
+      f.input :full_name
+      f.input :phone_number
+      f.input :email
+      f.input :notes
+      f.input :image, as: :file   # upload works fine
+    end
+    f.actions
+  end
 end
